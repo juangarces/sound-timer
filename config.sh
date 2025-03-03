a62cbfee73940283bd2b2f315dde4e341ca61ab3
juangarces <juangarces.g@gmail.com>
2025-03-03 08:26:55 +0000

    Refactor and delete unnecesary variable

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 # Run install.sh if you change name of command
 NEW_COMMAND="stimer"
-SCRIPT_FILE="sound-timer.sh"
 
 # Sound files
 SOUND_1MIN="./sounds/1.wav"
@@ -9,6 +8,7 @@ SOUND_15MIN="./sounds/15.wav"
 
 # Define the process ID file
 PID_FILE="/tmp/sound-timer.pid"
+LOG_FILE="/tmp/sound-timer.log"
 
-logging_enabled=false
+LOGGING_ENABLED="false"
 
